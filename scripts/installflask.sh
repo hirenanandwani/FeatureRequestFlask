7d106f0258d1836d8168282e873d789c906d4453
hirenanandwani <hirenanandwani@gmail.com>
2018-08-15 13:52:47 -0400

    Add files via upload

---
+++ scripts/installflask.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install python-virtualenv 
+sudo apt-get install python-pip
+
+mkdir flask-application
+cd flask-application
+virtualenv flaskenv
+
+source flaskenv/bin/activate
+
+pip install Flask
+sudo apt-get install python-flask-sqlalchemy
+
