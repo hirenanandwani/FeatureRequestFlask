da02b84f6a25b03b9d9bc46b49bf37f3e8a0c85e
hirenanandwani <hirenanandwani@gmail.com>
2018-08-28 12:27:57 +0530

    Update installflask.sh

--- scripts/installflask.sh
+++ scripts/installflask.sh
@@ -4,20 +4,10 @@ yes | sudo apt-get update
 yes | sudo apt-get install python-virtualenv 
 yes | sudo apt-get install python-pip
 
-#mkdir flask-application
-#cd flask-application
-virtualenv flaskenv
-
-pwd
-pwd
-
-
-source flaskenv/bin/activate
-
 yes | pip install Flask
 yes | sudo apt-get install python-flask-sqlalchemy
 
 cd /home/ubuntu/flaskapp
-rm -rf *
+#rm -rf *
 #sqlite3 features.db
 
